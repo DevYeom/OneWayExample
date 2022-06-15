6403ca6b9c9f4f9933847b0050ebcae32281b064
SeungYeop Yeom <dev.yeom@gmail.com>
2022-06-15 09:47:19 +0900

    Initial commit

---
+++ OneWayExample/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  OneWayExample
+//
+//  Created by DevYeom on 2022/06/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.systemBackground
+    }
+
+}
+
